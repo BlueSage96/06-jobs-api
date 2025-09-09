def6d41206812c86dda392d910e2cba89147990a
Brittany Price <bprice0496@gmail.com>
2025-09-09 01:27:18 -0400

    Answers for week 9

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
-require('dotenv').config();
-require('express-async-errors');
-const express = require('express');
+process.noDeprecation = true; //suppress deprecation warnings in console
+require("dotenv").config();
+require("express-async-errors");
+const express = require("express");
 const app = express();
 
+// connectDB
+const connectDB = require("./db/connect");
+const authenticateUser = require('./middleware/authentication');
+
+// routers
+const authRouter = require("./routes/auth");
+const gameRouter = require("./routes/game");
+
 // error handler
-const notFoundMiddleware = require('./middleware/not-found');
-const errorHandlerMiddleware = require('./middleware/error-handler');
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 
 app.use(express.json());
 // extra packages
 
 // routes
-app.get('/', (req, res) => {
-  res.send('jobs api');
-});
+app.use("/api/v1/sudoku/auth", authRouter);
+app.use("/api/v1/sudoku/game", authenticateUser, gameRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
@@ -22,6 +30,8 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
+   // await connectDB(process.env.MONGO_URI);
+   await connectDB(process.env.MONGO_URI)
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
